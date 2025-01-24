36ce8c2c21986688063030beb1ac4a3c36195c8f
Stavros Syrmpopoulos <87983517+sirbo44@users.noreply.github.com>
2025-01-24 02:33:54 +0200

    Delete enterprise_system_development-main/library_webApplication directory

--- enterprise_system_development-main/library_webApplication/src/main/java/com/example/library_webapplication/repos/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.library_webapplication.repos;
-
-import com.example.library_webapplication.entities.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-  Author findAuthorById(Integer id);
-}
